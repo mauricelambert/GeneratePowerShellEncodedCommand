72e545c413d3a67794bafb820f5247f45d1330ad
mauricelambert <50479118+mauricelambert@users.noreply.github.com>
2023-03-16 22:43:50 +0000

    Black style python code

--- generate_powersehll_command.py
+++ generate_powersehll_command.py
@@ -23,21 +23,46 @@ from sys import argv, stdin, stderr, exit, stdout
 from argparse import ArgumentParser, FileType
 from base64 import b64encode
 
-parser = ArgumentParser(description="This script generates a powershell command to execute script with only one command line (in memory).")
-parser.add_argument('-f', '--file', default=stdin, const=stdin, type=FileType(), nargs="?", help="Filename of the script to encode or stdin")
-parser.add_argument('-i', '--inputs', nargs="+", action="extend", help="Inputs to read in your powershell.")
-parser.add_argument('-p', '--as-process', action="store_true", help="Start this command as single process.")
+parser = ArgumentParser(
+    description="This script generates a powershell command to execute script with only one command line (in memory)."
+)
+parser.add_argument(
+    "-f",
+    "--file",
+    default=stdin,
+    const=stdin,
+    type=FileType(),
+    nargs="?",
+    help="Filename of the script to encode or stdin",
+)
+parser.add_argument(
+    "-i",
+    "--inputs",
+    nargs="+",
+    action="extend",
+    help="Inputs to read in your powershell.",
+)
+parser.add_argument(
+    "-p",
+    "--as-process",
+    action="store_true",
+    help="Start this command as single process.",
+)
 arguments = parser.parse_args()
 
 script = arguments.file.read()
 arguments.file.close()
 
 if arguments.as_process:
-	stdout.write(r'C:\Windows\System32\cmd.exe /c "')
+    stdout.write(r'C:\Windows\System32\cmd.exe /c "')
 
 if arguments.inputs:
-	inputs = '(echo ' + ' & echo '.join(arguments.inputs) + ') | '
-	stdout.write(inputs)
+    inputs = "(echo " + " & echo ".join(arguments.inputs) + ") | "
+    stdout.write(inputs)
 
-print(r"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -EncodedCommand", b64encode(script.encode('utf-16-le')).decode() + ('"' if arguments.as_process else ''))
+print(
+    r"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -EncodedCommand",
+    b64encode(script.encode("utf-16-le")).decode()
+    + ('"' if arguments.as_process else ""),
+)
 exit(0)
